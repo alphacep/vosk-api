859182c45be639130706d153fbe8c1589d94770c
Denis Treskunov <denis.treskunov@gmail.com>
2020-02-04 11:44:08 -0800

    really skip python 3.4 this time

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -e -x
-shopt -s extglob
 
 export KALDI_ROOT=/opt/kaldi
 
 # Compile wheels
-for pypath in /opt/python/cp3!(4); do
+for pypath in /opt/python/cp3*; do
+    if [[ $pypath == *cp34* ]]; then
+        echo "Skipping building wheel for deprecated Python version ${pypath}"
+        continue
+    fi
     export WHEEL_FLAGS=`${pypath}/bin/python3-config --cflags`
     mkdir -p /opt/wheelhouse
     "${pypath}/bin/pip3" install auditwheel
