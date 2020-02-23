174bf7b0ff01671327354361c793d8e5a4de0bab
Nickolay Shmyrev <nshmyrev@gmail.com>
2020-02-23 15:06:06 +0100

    Minor fixes

--- travis/build-dockcross.sh
+++ travis/build-dockcross.sh
@@ -5,4 +5,4 @@ set -x
 
 docker build --build-arg="DOCKCROSS_IMAGE=linux-armv7" --build-arg="OPENBLAS_ARCH=ARMV7" --file Dockerfile.dockcross --tag alphacep/kaldi-dockcross-armv7:latest .
 docker build --build-arg="DOCKCROSS_IMAGE=linux-armv6" --build-arg="OPENBLAS_ARCH=ARMV6" --file Dockerfile.dockcross --tag alphacep/kaldi-dockcross-armv6:latest .
-docker build --build-arg="DOCKCROSS_IMAGE=linux-arm64" --build-arg="OPENBLAS_ARCH=ARMV7" --file Dockerfile.dockcross --tag alphacep/kaldi-dockcross-arm64:latest .
+docker build --build-arg="DOCKCROSS_IMAGE=linux-arm64" --build-arg="OPENBLAS_ARCH=ARMV8" --file Dockerfile.dockcross --tag alphacep/kaldi-dockcross-arm64:latest .
--- travis/build-wheels-dockcross.sh
+++ travis/build-wheels-dockcross.sh
@@ -4,15 +4,16 @@ set -e -x
 export KALDI_ROOT=/opt/kaldi
 export WHEEL_FLAGS=`$CROSS_ROOT/bin/python3-config --cflags`
 export PATH=/opt/python/cp3.7-cp3.7m/bin:$PATH
+echo $CROSS_TRIPLE
 case $CROSS_TRIPLE in
-    *armv6*)
+    *arm-*)
         export _PYTHON_HOST_PLATFORM=linux-armv6l
         ;;
-    *armv7*)
+    *armv7-*)
         export _PYTHON_HOST_PLATFORM=linux-armv7l
         ;;
-    *arm64*)
-        export _PYTHON_HOST_PLATFORM=linux-arm64
+    *aarch64-*)
+        export _PYTHON_HOST_PLATFORM=linux-aarch64
         ;;
 esac
 
