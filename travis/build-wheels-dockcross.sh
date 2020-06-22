4333c3c242c9b83af33a8d1f6403e4efbd291aa3
Nickolay Shmyrev <nshmyrev@gmail.com>
2020-06-23 01:11:07 +0200

    Build dockcross with python 3.8

--- travis/build-wheels-dockcross.sh
+++ travis/build-wheels-dockcross.sh
@@ -2,23 +2,30 @@
 set -e -x
 
 ORIG_PATH=$PATH
-for pyver in 3.6 3.7; do
+for pyver in 3.6 3.7 3.8; do
 
     export KALDI_ROOT=/opt/kaldi
     export PATH=/opt/python/cp${pyver}-cp${pyver}m/bin:$ORIG_PATH
     export VOSK_SOURCE=/io/src
+
+    # Python 3.8 somehow changed syconfig file name
+    sysconfig_bit="m"
+    if [ $pyver == "3.8" ]; then
+         sysconfig_bit=""
+    fi
+
     case $CROSS_TRIPLE in
         *arm-*)
             export _PYTHON_HOST_PLATFORM=linux-armv6l
-            export _PYTHON_SYSCONFIGDATA_NAME=_sysconfigdata_m_linux_arm-linux-gnueabihf
+            export _PYTHON_SYSCONFIGDATA_NAME=_sysconfigdata_${sysconfig_bit}_linux_arm-linux-gnueabihf
             ;;
         *armv7-*)
             export _PYTHON_HOST_PLATFORM=linux-armv7l
-            export _PYTHON_SYSCONFIGDATA_NAME=_sysconfigdata_m_linux_arm-linux-gnueabihf
+            export _PYTHON_SYSCONFIGDATA_NAME=_sysconfigdata_${sysconfig_bit}_linux_arm-linux-gnueabihf
             ;;
         *aarch64-*)
             export _PYTHON_HOST_PLATFORM=linux-aarch64
-            export _PYTHON_SYSCONFIGDATA_NAME=_sysconfigdata_m_linux_aarch64-linux-gnu
+            export _PYTHON_SYSCONFIGDATA_NAME=_sysconfigdata_${sysconfig_bit}_linux_aarch64-linux-gnu
             ;;
     esac
     export PYTHONHOME=$CROSS_ROOT
