f97383c17f463d5ac2ea49ebf430f7f25a1dc0f5
Nickolay Shmyrev <nshmyrev@gmail.com>
2020-09-22 00:00:24 +0200

    Update models location

--- python/example/test_simple.py
+++ python/example/test_simple.py
@@ -8,7 +8,7 @@ import wave
 SetLogLevel(0)
 
 if not os.path.exists("model"):
-    print ("Please download the model from https://github.com/alphacep/vosk-api/blob/master/doc/models.md and unpack as 'model' in the current folder.")
+    print ("Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
     exit (1)
 
 wf = wave.open(sys.argv[1], "rb")
