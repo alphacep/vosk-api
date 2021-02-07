02dc0ce0c8709a924137a0a7e7c3925c6e6ac84f
Nickolay Shmyrev <nshmyrev@gmail.com>
2021-02-07 15:17:35 +0100

    Take source location from environment

--- python/vosk_builder.py
+++ python/vosk_builder.py
@@ -3,9 +3,12 @@
 import os
 from cffi import FFI
 
+vosk_root=os.environ.get("VOSK_SOURCE", "..")
+cpp_command = "cpp " + vosk_root + "/src/vosk_api.h"
+
 ffibuilder = FFI()
 ffibuilder.set_source("vosk.vosk_cffi", None)
-ffibuilder.cdef(os.popen("cpp ../src/vosk_api.h").read())
+ffibuilder.cdef(os.popen(cpp_command).read())
 
 if __name__ == '__main__':
     ffibuilder.compile(verbose=True)
