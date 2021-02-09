e5cc2739e21f4ffc92bb3ad2063f42c82b55c478
Nickolay Shmyrev <nshmyrev@gmail.com>
2021-02-09 23:36:32 +0100

    Copy out lib files too

--- travis/build-wheels-dockcross.sh
+++ travis/build-wheels-dockcross.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e -x
 
+# Build so file
 cd /opt
 git clone https://github.com/alphacep/vosk-api
 cd /opt/vosk-api/src
 KALDI_ROOT=/opt/kaldi make -j $(nproc)
 
+# Decide architecture name
 export VOSK_SOURCE=/opt/vosk-api
 case $CROSS_TRIPLE in
     *armv7-*)
@@ -15,4 +17,10 @@ case $CROSS_TRIPLE in
         export VOSK_ARCHITECTURE=aarch64
         ;;
 esac
+
+# Copy library to output folder
+mkdir -p /io/wheelhouse/linux-$VOSK_ARCHITECTURE
+cp /opt/vosk-api/src/*.so /io/wheelhouse/linux-$VOSK_ARCHITECTURE
+
+# Build wheel
 pip3 wheel /opt/vosk-api/python --no-deps -w /io/wheelhouse
