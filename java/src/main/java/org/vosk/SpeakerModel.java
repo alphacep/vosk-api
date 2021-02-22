a09f431c9438ff9332d71a5011909c4f6e99f2ba
Peter Kronenberg <peter.kronenberg@torch.ai>
2021-02-22 09:35:45 -0500

    Convert tabs to spaces

--- java/src/main/java/org/vosk/SpeakerModel.java
+++ java/src/main/java/org/vosk/SpeakerModel.java
@@ -3,9 +3,9 @@ package org.vosk;
 import com.sun.jna.PointerType;
 
 public class SpeakerModel extends PointerType implements AutoCloseable {
-	public SpeakerModel() {
-	}
-	
+    public SpeakerModel() {
+    }
+
     public SpeakerModel(String path) {
         super(LibVosk.vosk_spk_model_new(path));
     }
