d0ae9fafccd158e84da229516e882e925656ec42
Peter Kronenberg <peter.kronenberg@torch.ai>
2021-03-31 11:23:29 -0400

    Allow libvosk.dll to get loaded on Windows

--- java/lib/src/main/java/org/vosk/LibVosk.java
+++ java/lib/src/main/java/org/vosk/LibVosk.java
@@ -1,12 +1,14 @@
 package org.vosk;
 
 import com.sun.jna.Native;
+import com.sun.jna.Platform;
 import com.sun.jna.Pointer;
 
 public class LibVosk {
 
     static {
-        Native.register("vosk");
+
+        Native.register(Platform.isWindows() ? "libvosk" : "vosk");
     }
 
     public static native void vosk_set_log_level(int level);
