3990bb7a3aa04b7d673380ac9b63753e48458e29
sadrasabouri <sabouri.sadra@gmail.com>
2021-04-30 08:18:01 +0430

    edit : minor edits in test_ffmpeg.js.

--- nodejs/demo/test_ffmpeg.js
+++ nodejs/demo/test_ffmpeg.js
@@ -2,11 +2,11 @@ var vosk = require('..')
 
 const fs = require("fs");
 const { spawn } = require("child_process");
-const Stream = require('stream');
 
 MODEL_PATH = "model"
 FILE_NAME = "test.wav"
-SAMPLE_RATE = 16000 
+SAMPLE_RATE = 16000
+BUFFER_SIZE = 4000
 
 if (!(fs.existsSync(MODEL_PATH) && fs.lstatSync(MODEL_PATH).isDirectory())){
     console.log("Please download the model from https://alphacephei.com/vosk/models and unpack as " + MODEL_PATH + " in the current folder.")
@@ -22,18 +22,12 @@ const rec = new vosk.Recognizer({model: model, sampleRate: SAMPLE_RATE});
 
 const ffmpeg_run = spawn('ffmpeg', ['-loglevel', 'quiet', '-i', FILE_NAME,
                          '-ar', String(SAMPLE_RATE) , '-ac', '1',
-                         '-f', 's16le', '-']);
+                         '-f', 's16le', '-bufsize', String(BUFFER_SIZE) , '-']);
 
 ffmpeg_run.stdout.on('data', (stdout) => {
-    const wave_stream = Stream.Readable.from(stdout)
-    while (true){
-        const data = wave_stream.read(4000)
-        if (data == null)
-            break;
-        if (rec.acceptWaveform(data))
+        if (rec.acceptWaveform(stdout))
             console.log(rec.result());
         else
             console.log(rec.partialResult());
-    }
     console.log(rec.finalResult());
 });
