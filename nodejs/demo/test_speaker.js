fdeba607e47f6689e6cd99258397f362de910b05
Nickolay Shmyrev <nshmyrev@gmail.com>
2021-05-02 21:44:54 +0200

    Reformat and simplify code a bit

--- nodejs/demo/test_speaker.js
+++ nodejs/demo/test_speaker.js
@@ -24,28 +24,28 @@ if (process.argv.length > 2)
 const model = new vosk.Model(MODEL_PATH);
 const speakerModel = new vosk.SpeakerModel(SPEAKER_MODEL_PATH);
 
-const wfStream = fs.createReadStream(FILE_NAME, { highWaterMark: 4096 });
 const wfReader = new wav.Reader();
 const wfReadable = new Readable().wrap(wfReader);
 
 wfReader.on('format', async ({ audioFormat, sampleRate, channels }) => {
-  if (audioFormat != 1 || channels != 1) {
-      console.error('Audio file must be WAV format mono PCM.');
-      process.exit(1);
-  }
-  const rec = new vosk.Recognizer({ model: model,
-                                    speakerModel: speakerModel,
-                                    sampleRate: sampleRate });
-  for await (const data of wfReadable) {
-      const end_of_speech = rec.acceptWaveform(data);
-      if (end_of_speech) {
-        console.log(rec.finalResult());
-      }
-  }
-  console.log(rec.finalResult());
-  rec.free();
+    if (audioFormat != 1 || channels != 1) {
+        console.error('Audio file must be WAV format mono PCM.');
+        process.exit(1);
+    }
+    const rec = new vosk.Recognizer({ model: model,
+                                      speakerModel: speakerModel,
+                                      sampleRate: sampleRate });
+    for await (const data of wfReadable) {
+        const end_of_speech = rec.acceptWaveform(data);
+        if (end_of_speech) {
+             console.log(rec.finalResult());
+        }
+    }
+    console.log(rec.finalResult());
+    rec.free();
 });
-wfStream.pipe(wfReader).on('finish', function (err) {
+
+fs.createReadStream(FILE_NAME, { highWaterMark: 4096 }).pipe(wfReader).on('finish', function (err) {
     model.free();
     speakerModel.free();
 });
