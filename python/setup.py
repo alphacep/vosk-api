50a316edc6527a61703f11c794a835d8718c1975
Nickolay Shmyrev <nshmyrev@gmail.com>
2021-05-05 19:34:30 +0200

    RNNLM rescoring support

--- python/setup.py
+++ python/setup.py
@@ -44,7 +44,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vosk",
-    version="0.3.22",
+    version="0.3.27",
     author="Alpha Cephei Inc",
     author_email="contact@alphacephei.com",
     description="Offline open source speech recognition API based on Kaldi and Vosk",
