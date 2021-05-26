6aa5af7640fa95bb7fe75b322a5249ca065a1471
Nickolay Shmyrev <nshmyrev@gmail.com>
2021-05-26 21:22:07 +0200

    Add reset test

---
+++ python/example/test_reset.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from vosk import Model, KaldiRecognizer, SetLogLevel
+import sys
+import os
+import wave
+import json
+
+SetLogLevel(0)
+
+if not os.path.exists("model"):
+    print ("Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
+    exit (1)
+
+wf = wave.open(sys.argv[1], "rb")
+if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
+    print ("Audio file must be WAV format mono PCM.")
+    exit (1)
+
+model = Model("model")
+rec = KaldiRecognizer(model, wf.getframerate())
+
+while True:
+    data = wf.readframes(4000)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        print(rec.Result())
+        break
+
+    else:
+        jres = json.loads(rec.PartialResult())
+        print(jres)
+
+        if jres['partial'] == "one zero zero zero":
+            print("We can reset recognizer here and start over")
+            rec.Reset();
