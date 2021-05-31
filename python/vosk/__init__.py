149ff7a9634572159a11ba5249e4d8125a8bcf7c
sadrasabouri <sabouri.sadra@gmail.com>
2021-05-31 23:44:42 +0430

    add : PldaScoring added to __init__.py.

--- python/vosk/__init__.py
+++ python/vosk/__init__.py
@@ -72,6 +72,9 @@ class KaldiRecognizer(object):
     def FinalResult(self):
         return _ffi.string(_c.vosk_recognizer_final_result(self._handle)).decode('utf-8')
 
+    def PldaScoring(self):
+        return _c.vosk_recognizer_plda_scoring(self._handle)
+
     def Reset(self):
         return _c.vosk_recognizer_reset(self._handle)
 
