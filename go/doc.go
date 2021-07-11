915dcab59706ff353e6350170487bdcfb0b53dab
Nickolay Shmyrev <nshmyrev@gmail.com>
2021-07-11 15:54:42 +0200

    Add doc

---
+++ go/doc.go
@@ -0,0 +1,2 @@
+// Go bindings for Vosk. Vosk is an offline open source speech recognition API for Android, iOS, Raspberry Pi and servers. It enables speech recognition models for 18 languages and dialects - English, Indian English, German, French, Spanish, Portuguese, Chinese, Russian, Turkish, Vietnamese, Italian, Dutch, Catalan, Arabic, Greek, Farsi, Filipino, Ukrainian.
+package vosk
