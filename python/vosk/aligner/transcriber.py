7dc390620d2afdde16285fa2d9bd16a0d1dce7ff
Vadim Doroshenko <vdk231190@mail.ru>
2021-11-11 17:14:20 +0100

    changed transcriber.py fixed frames duration (add mul by 2), deleted duration as parametr(input/output), set realign case audiofile start position as tell(), splited lines for more readable script

--- python/vosk/aligner/transcriber.py
+++ python/vosk/aligner/transcriber.py
@@ -9,32 +9,45 @@ class Transcriber:
         realign part using small personal LM/AM
     Output recognized words 
     '''
-    def transcribe(recognizer, wavfile, chunk_end=None, duration=None):
-        read_frames = 0
-        if chunk_end == None and duration == None: # case of align
+    def transcribe(recognizer, wavfile, chunk_end=None):
+
+        if chunk_end == None: # case of align
+            read_frames = 0
             duration = wavfile.getnframes() / float(wavfile.getframerate())
-            frames = int(duration * wavfile.getframerate())
+            frames = int(2 * duration * wavfile.getframerate())
+            
             if wavfile.getnchannels() != 1 or wavfile.getsampwidth() != 2 or wavfile.getcomptype() != "NONE":
                 print("Audio file must be WAV format mono PCM.")
                 exit (1)
-        else: # case of realign
-            duration = duration
+
+        else: # case of realign         
             frames = chunk_end
+            read_frames = wavfile.tell()
+        
         result = [] 
         recognizer.SetWords(True)
+        
         while True:
             buf = wavfile.readframes(4000) # getting parts of wavfile
             read_frames = read_frames + len(buf)
+            
             if len(buf) == 0:
                 break
-            if read_frames > frames and chunk_end is not None:
-                    break
+            
+            if read_frames > frames:
+                break
+            
             if recognizer.AcceptWaveform(buf):
                 res = json.loads(recognizer.Result())
+                
                 if 'result' in res:
                     result.extend(res['result'])
+
         res = json.loads(recognizer.FinalResult())
+        
         if 'result' in res:
             result.extend(res['result'])
+
         words = result
-        return words, duration
+        
+        return words
