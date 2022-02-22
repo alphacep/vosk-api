35219ac21bb0d8cd0f1ee40729d138e896575afd
Vadim Doroshenko <vdk231190@mail.ru>
2022-02-22 18:10:01 +0100

    directory processing was added: method process_dirs in transcriber.py and isdir condition with mutiple processing in vosk_transcriber.py

--- python/transcriber/transcriber.py
+++ python/transcriber/transcriber.py
@@ -2,9 +2,11 @@ import json
 import sys
 import subprocess
 import time
+import os
 
 from datetime import datetime as dt
 from vosk import KaldiRecognizer
+
 
 class Transcriber:
 
@@ -41,4 +43,17 @@ class Transcriber:
         script_time = str(dt.now() - start_time)
         seconds = script_time[5:7].strip('0')
         mcseconds = script_time[8:].strip('0')
-        return script_time.strip(':0'), seconds, mcseconds 
+        return script_time.strip(':0'), seconds, mcseconds
+
+    def process_dirs(model, inputdata, outputdata, outputtype, log):
+        files = os.listdir(inputdata)
+        arg_list = list()
+        input_dir = inputdata + '/'
+        output_dir = outputdata + '/'
+        extension_i = f".{files[0].split('.')[1]}"
+        format_o = f".{outputtype}"
+        input_list = [input_dir + each for each in files]
+        output_list = [output_dir + each.replace(extension_i, format_o) for each in files]
+        for i in range(len(input_list)):
+            arg_list.extend([(model, input_list[i], output_list[i], outputtype, log)])
+        return arg_list
--- python/transcriber/vosk_transcriber.py
+++ python/transcriber/vosk_transcriber.py
@@ -1,11 +1,11 @@
 import os
-import wave
 import logging
 import sys
 import argparse
 
 from transcriber import Transcriber as transcriber
 from vosk import Model
+from multiprocessing.dummy import Pool
 
 
 parser = argparse.ArgumentParser(
@@ -14,17 +14,14 @@ parser.add_argument(
         'model', type=str,
         help='model path')
 parser.add_argument(
-        '-i', '--input', metavar='inputfile', type=str,
+        '-i', '--input', type=str,
         help='optional arg audiofile')
 parser.add_argument(
-        '-o', '--output', metavar='outputfile', type=str,
+        '-o', '--output', metavar='outputdata', type=str,
         help='output filename path')
 parser.add_argument(
-        '-d', '--folder', type=str,
-        help='optional arg input folder with audiofiles')
-parser.add_argument(
-        '-od', '--output folder', type=str,
-        help='optional arg output folder with transcription files')
+        '-ot', '--outputtype', type=str,
+        help='output data type without dot')
 parser.add_argument(
         '--log', default='INFO',
         help='logging level')
@@ -34,29 +31,39 @@ log_level = args.log.upper()
 logging.getLogger().setLevel(log_level)
 logging.info('checking input args')
 
-def main(model, inputfile, outputfile, options, log):
-    
-    logging.info('checking model')
-    if not os.path.exists('model'):
-        print("Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
-
-    model = Model('model') 
+def calculate(model, inputdata, outputdata, outputtype, log):
     logging.info('converting audiofile to 16K sampled wav')
-    wf = transcriber.resample_ffmpeg(inputfile)
+    wf = transcriber.resample_ffmpeg(inputdata)
     logging.info('converting complite')
-    logging.info('starting transcription')
+    logging.info('startting transcription')
     final_result, tot_samples = transcriber.transcribe(model, wf)
     logging.info('transcription complite')
-    fh = open(outputfile, 'w', encoding='utf-8') if outputfile else sys.stdout
+    fh = open(outputdata, 'w', encoding='utf-8') if outputdata else sys.stdout
     fh.write(final_result)
-
-    if outputfile:
-        logging.info('output written to %s' % (outputfile))        
+    if outputdata:
+        logging.info('output writen to %s' % (outputdata))
     logging.info('program finished')
+
+    return final_result, tot_samples
+
+def main(model, inputdata, outputdata, outputtype, log):
+    logging.info('checking model')
+    model = Model('model')
+    if not os.path.exists('model'):
+        print("Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
+    
+    if os.path.isdir(inputdata) and os.path.isdir(outputdata) and outputtype:
+        arg_list = transcriber.process_dirs(model, inputdata, outputdata, outputtype, log)
+        with Pool() as pool:
+            for final_result, tot_samples in pool.starmap(calculate, arg_list):
+                return final_result, tot_samples
+    
+    elif os.path.isfile(inputdata):
+        final_result, tot_samples = calculate(model, inputdata, outputdata, outputtype, log)
     return final_result, tot_samples
 
 if __name__ == '__main__':
     start_time = transcriber.get_time()
-    tot_samples = main(args.model, args.input, args.output, args.folder, args.log)[1]
+    tot_samples = main(args.model, args.input, args.output, args.outputtype, args.log)[1]
     diff_end_start, sec, mcsec = transcriber.send_time(start_time)
     print('\n'f'''Script info: execution time: {sec} sec, {mcsec} mcsec; xRT: {format(tot_samples / 16000.0 / float(diff_end_start), '.3f')}''')
