354afa3f83e4a112baf084058553e87751d8be48
Vadim Doroshenko <vdk231190@mail.ru>
2022-03-09 14:55:19 +0100

    added new example moon.wav instead of cats.wav; added srt transcribe

--- python/transcriber/transcriber.py
+++ python/transcriber/transcriber.py
@@ -3,49 +3,77 @@ import sys
 import subprocess
 import time
 import os
+import requests
+import urllib.request
+import zipfile
+import srt
+import datetime
 
 from datetime import datetime as dt
 from vosk import KaldiRecognizer
 
 
 class Transcriber:
-
-    def transcribe(model, wf):
+    
+    def transcribe(model, process, outputdata, outputtype):
         rec = KaldiRecognizer(model, 16000)
+        WORDS_PER_LINE = 7
         tot_samples = 0
         final_result = ''
-    
+        result = list()
+        subs = list()
         while True:
-            data = wf.stdout.read(4000)
+            data = process.stdout.read(4000)
             if len(data) == 0:
                 break
-
-            if rec.AcceptWaveform(data):
-                tot_samples += len(data)
-                result = json.loads(rec.Result())
-                final_result += result['text'] + ' '
-
+            if outputtype == 'txt':
+                if rec.AcceptWaveform(data):
+                    tot_samples += len(data)
+                    result.append(json.loads(rec.Result()))
+            elif outputtype == 'srt':
+                rec.SetWords(True)
+                if rec.AcceptWaveform(data):
+                    tot_samples += len(data)
+                    result.append(json.loads(rec.Result()))
+        result.append(json.loads(rec.FinalResult()))
+        if outputtype == 'srt':
+            for i, res in enumerate(result):
+                if not 'result' in res:
+                    continue
+                words = res['result']
+                for j in range(0, len(words), WORDS_PER_LINE):
+                    line = words[j : j + WORDS_PER_LINE]
+                    s = srt.Subtitle(index=len(subs),
+                            content = ' '.join([l['word'] for l in line]),
+                            start=datetime.timedelta(seconds=line[0]['start']),
+                            end=datetime.timedelta(seconds=line[-1]['end']))
+                    subs.append(s)
+        if outputtype == 'srt':
+            final_result = srt.compose(subs)
+        elif outputtype == 'txt':
+            for i in range(len(result)):
+                final_result += result[i]['text'] + ' '
         return final_result, tot_samples
-
+    
     def resample_ffmpeg(infile):
         process = subprocess.Popen(
-            ['ffmpeg', '-loglevel', 'quiet', '-i', 
+            ['ffmpeg', '-nostdin', '-loglevel', 'quiet', '-i', 
             infile, 
             '-ar', '16000','-ac', '1', '-f', 's16le', '-'], 
             stdout=subprocess.PIPE)
         return process
-
+    
     def get_time():
         start_time = dt.now()
         return start_time
-
+    
     def send_time(start_time):
         script_time = str(dt.now() - start_time)
-        seconds = script_time[5:7].strip('0')
+        seconds = script_time[5:8].strip('0')
         mcseconds = script_time[8:].strip('0')
-        return script_time.strip(':0'), seconds, mcseconds
-
-    def process_dirs(model, inputdata, outputdata, outputtype, log):
+        return script_time.strip(':0'), seconds.rstrip('.'), mcseconds
+    
+    def process_dir(model, inputdata, outputdata, outputtype, log):
         files = os.listdir(inputdata)
         arg_list = list()
         input_dir = inputdata + '/'
--- python/transcriber/vosk_transcriber.py
+++ python/transcriber/vosk_transcriber.py
@@ -14,18 +14,18 @@ parser.add_argument(
         'model', type=str,
         help='model path')
 parser.add_argument(
-        '-i', '--input', type=str,
-        help='optional arg audiofile')
+        'input', type=str,
+        help='audiofile')
 parser.add_argument(
-        '-o', '--output', metavar='outputdata', type=str,
-        help='output filename path')
+        '-output', default='', type=str,
+        help='optional output filename path')
 parser.add_argument(
-        '-ot', '--outputtype', type=str,
-        help='output data type without dot')
+        '-otype', '--outputtype', default='txt', type=str,
+        help='optional arg output data type')
 parser.add_argument(
         '--log', default='INFO',
         help='logging level')
-
+    
 args = parser.parse_args()
 log_level = args.log.upper()
 logging.getLogger().setLevel(log_level)
@@ -33,37 +33,37 @@ logging.info('checking input args')
 
 def calculate(model, inputdata, outputdata, outputtype, log):
     logging.info('converting audiofile to 16K sampled wav')
-    wf = transcriber.resample_ffmpeg(inputdata)
-    logging.info('converting complite')
-    logging.info('startting transcription')
-    final_result, tot_samples = transcriber.transcribe(model, wf)
-    logging.info('transcription complite')
-    fh = open(outputdata, 'w', encoding='utf-8') if outputdata else sys.stdout
-    fh.write(final_result)
+    process = transcriber.resample_ffmpeg(inputdata)
+    logging.info('complite')
+    logging.info('starting transcription')
+    final_result, tot_samples= transcriber.transcribe(model, process, outputdata, outputtype)
+    logging.info('complite')
     if outputdata:
+        with open(outputdata, 'w', encoding='utf-8') as fh:
+            fh.write(final_result)
         logging.info('output writen to %s' % (outputdata))
-    logging.info('program finished')
-
+    else:
+        print(final_result)
     return final_result, tot_samples
-
+    
 def main(model, inputdata, outputdata, outputtype, log):
-    logging.info('checking model')
-    model = Model('model')
-    if not os.path.exists('model'):
-        print("Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
-    
-    if os.path.isdir(inputdata) and os.path.isdir(outputdata) and outputtype:
-        arg_list = transcriber.process_dirs(model, inputdata, outputdata, outputtype, log)
-        with Pool() as pool:
-            for final_result, tot_samples in pool.starmap(calculate, arg_list):
-                return final_result, tot_samples
-    
+    try:
+        logging.info('checking model')
+        model = Model('model')
+    except Exception as e:
+        logging.info("There is no model in the current folder")
+        exit(1)
+    if os.path.isdir(inputdata) and os.path.isdir(outputdata):
+            arg_list = transcriber.process_dir(model, inputdata, outputdata, outputtype, log)
+            with Pool() as pool:
+                for final_result, tot_samples in pool.starmap(calculate, arg_list):
+                    return final_result, tot_samples
     elif os.path.isfile(inputdata):
         final_result, tot_samples = calculate(model, inputdata, outputdata, outputtype, log)
     return final_result, tot_samples
-
+    
 if __name__ == '__main__':
     start_time = transcriber.get_time()
     tot_samples = main(args.model, args.input, args.output, args.outputtype, args.log)[1]
     diff_end_start, sec, mcsec = transcriber.send_time(start_time)
-    print('\n'f'''Script info: execution time: {sec} sec, {mcsec} mcsec; xRT: {format(tot_samples / 16000.0 / float(diff_end_start), '.3f')}''')
+    print(f'''Script info: execution time: {sec} sec, {mcsec} mcsec; xRT: {format(tot_samples / 16000.0 / float(diff_end_start), '.3f')}''')
