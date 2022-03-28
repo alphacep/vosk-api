1f05e700fcb1771a10cf656ab873b52e2f2f9f05
Vadim Doroshenko <vdk231190@mail.ru>
2022-03-28 18:18:58 +0200

    corrected remarks: naming, changed area of variables using, declared some variables as constans, changed spot of calculate(old)-get_result(new) method inside of main, changed type transmitted args between scripts and methods, changed function from print to log for output script info(ececution time and xRT)

--- python/transcriber/transcriber.py
+++ python/transcriber/transcriber.py
@@ -6,36 +6,42 @@ import urllib.request
 import zipfile
 import srt
 import datetime
+import os
 
 from datetime import datetime as dt
 from vosk import KaldiRecognizer, Model
 from pathlib import Path
 
 
+WORDS_PER_LINE = 7
+model_pre_path = 'https://alphacephei.com/vosk/models/'
+model_list_url = model_pre_path + 'model-list.json'
+
 class Transcriber:
 
-    def transcribe(model, process, outputdata, outputtype):
+    def transcribe(model, process, args):
         rec = KaldiRecognizer(model, 16000)
-        WORDS_PER_LINE = 7
+        rec.SetWords(True)
         tot_samples = 0
-        final_result = ''
         result = list()
         subs = list()
+
+        def get_result_and_tot_samples(rec, data, tot_samples, result):
+            if rec.AcceptWaveform(data):
+                tot_samples += len(data)
+                result.append(json.loads(rec.Result()))
+            return result, tot_samples
+
         while True:
             data = process.stdout.read(4000)
             if len(data) == 0:
                 break
-            if outputtype == 'txt':
-                if rec.AcceptWaveform(data):
-                    tot_samples += len(data)
-                    result.append(json.loads(rec.Result()))
-            elif outputtype == 'srt':
-                rec.SetWords(True)
-                if rec.AcceptWaveform(data):
-                    tot_samples += len(data)
-                    result.append(json.loads(rec.Result()))
+            if args.outputtype == 'txt':
+                    result, tot_samples = get_result_and_tot_samples(rec, data, tot_samples, result)
+            elif args.outputtype == 'srt':
+                    result, tot_samples = get_result_and_tot_samples(rec, data, tot_samples, result)
         result.append(json.loads(rec.FinalResult()))
-        if outputtype == 'srt':
+        if args.outputtype == 'srt':
             for i, res in enumerate(result):
                 if not 'result' in res:
                     continue
@@ -47,9 +53,10 @@ class Transcriber:
                             start=datetime.timedelta(seconds=line[0]['start']),
                             end=datetime.timedelta(seconds=line[-1]['end']))
                     subs.append(s)
-        if outputtype == 'srt':
+        final_result = ''
+        if args.outputtype == 'srt':
             final_result = srt.compose(subs)
-        elif outputtype == 'txt':
+        elif args.outputtype == 'txt':
             for i in range(len(result)):
                 final_result += result[i]['text'] + ' '
         return final_result, tot_samples
@@ -72,56 +79,53 @@ class Transcriber:
         mcseconds = script_time[8:].strip('0')
         return script_time.strip(':0'), seconds.rstrip('.'), mcseconds
 
-    def process_dir(model, inputdata, outputdata, outputtype, log):
-        files = os.listdir(inputdata)
+    def process_dir(args):
+        files = os.listdir(args.input)
         arg_list = list()
-        input_dir = inputdata + '/'
-        output_dir = outputdata + '/'
+        input_dir = args.input + '/'
+        output_dir = args.output + '/'
         extension_i = f".{files[0].split('.')[1]}"
-        format_o = f".{outputtype}"
+        format_o = f".{args.outputtype}"
         input_list = [input_dir + each for each in files]
         output_list = [output_dir + each.replace(extension_i, format_o) for each in files]
-        [arg_list.extend([(model, input_list[i], output_list[i], outputtype, log)]) for i in range(len(input_list))]
+        [arg_list.extend([(input_list[i], output_list[i])]) for i in range(len(input_list))]
         return arg_list
 
     def get_list_models():
-        url = 'https://alphacephei.com/vosk/models/model-list.json'
-        response = requests.get(url)
+        response = requests.get(model_list_url)
         [print(response.json()[i]['name']) for i in range(len(response.json()))]
         exit(1)
 
-    def get_model(lang, model_name=None):
+    def get_model(args):
 
-        def check_model_path():
+        def get_model_path():
             model_path = Path.home() / '.cache' / 'vosk'
             if not Path.is_dir(model_path):
                 Path.mkdir(model_path)
             return model_path
 
         def download_model(result_model, model_path):
-            pre_link = 'https://alphacephei.com/vosk/models/'
-            model_zip = str(model_path) + '/' + result_model + '.zip'
-            model_url = pre_link + result_model + '.zip'
+            model_zip = Path(model_path, result_model + '.zip')
+            model_url = model_pre_path + result_model + '.zip'
             urllib.request.urlretrieve(model_url, model_zip)
             with zipfile.ZipFile(model_path / model_zip, 'r') as model_ref:
                 model_ref.extractall(model_path)
             Path.unlink(model_path / model_zip)
 
-        url = 'https://alphacephei.com/vosk/models/model-list.json'
         model_list = list()
-        response = requests.get(url)
+        response = requests.get(model_list_url)
         for i in range(len(response.json())):
-            if response.json()[i]['lang'] == lang:
+            if response.json()[i]['lang'] == args.lang:
                 if response.json()[i]['type'] == 'small' and response.json()[i]['obsolete'] == 'false':
-                    if model_name == None:
+                    if args.model_name == None:
                         result_model = response.json()[i]['name']
                     else:
-                        result_model = model_name
-        model_path = check_model_path()
-        model_arg = str(model_path) + '/' + result_model
-        if Path(model_arg).exists():
+                        result_model = args.model_name
+        model_path = Path(get_model_path())
+        model_location = Path(model_path, result_model)
+        if Path(model_location).exists():
             pass
         else:
             download_model(result_model, model_path)
-        model = Model(model_arg)
+        model = Model(str(model_location))
         return model
--- python/transcriber/vosk_transcriber.py
+++ python/transcriber/vosk_transcriber.py
@@ -21,7 +21,7 @@ parser.add_argument(
         '-lang',  default='en-us', type=str,
         help='smallest available model for selected language')
 parser.add_argument(
-        'input', type=str,
+        '-input', type=str,
         help='audiofile')
 parser.add_argument(
         '-output', default='', type=str,
@@ -38,36 +38,39 @@ log_level = args.log.upper()
 logging.getLogger().setLevel(log_level)
 logging.info('checking input args')
 
-def calculate(model, inputdata, outputdata, outputtype, log):
-    logging.info('converting audiofile to 16K sampled wav')
-    process = transcriber.resample_ffmpeg(inputdata)
-    logging.info('complite')
-    logging.info('starting transcription')
-    final_result, tot_samples= transcriber.transcribe(model, process, outputdata, outputtype)
-    logging.info('complite')
-    if outputdata:
-        with open(outputdata, 'w', encoding='utf-8') as fh:
-            fh.write(final_result)
-        logging.info('output writen to %s' % (outputdata))
-    else:
-        print(final_result)
-    return final_result, tot_samples
+def main(args):
+    global model
 
-def main(model, inputdata, outputdata, outputtype, list_models, model_name, lang, log):
-    model = transcriber.get_model(lang, model_name)
-    if list_models:
+    def get_results(inputdata):
+        logging.info('converting audiofile to 16K sampled wav')
+        process = transcriber.resample_ffmpeg(inputdata[0])
+        logging.info('complite')
+        logging.info('starting transcription')
+        final_result, tot_samples = transcriber.transcribe(model, process, args)
+        logging.info('complite')
+        if args.output:
+            with open(inputdata[1], 'w', encoding='utf-8') as fh:
+                fh.write(final_result)
+            logging.info('output written to %s' % (inputdata[1]))
+        else:
+            print(final_result)
+        return final_result, tot_samples
+
+    if args.list_models:
         transcriber.get_list_models()
-    if Path(inputdata).is_dir() and Path(outputdata).is_dir():
-        arg_list = transcriber.process_dir(model, inputdata, outputdata, outputtype, log)
+    model = transcriber.get_model(args)
+    if Path(args.input).is_dir() and Path(args.output).is_dir():
+        file_list = transcriber.process_dir(args)
         with Pool() as pool:
-            for final_result, tot_samples in pool.starmap(calculate, arg_list):
+            for final_result, tot_samples in pool.map(get_results, file_list):
                 return final_result, tot_samples
-    elif Path(inputdata).is_file():
-        final_result, tot_samples = calculate(model, inputdata, outputdata, outputtype, log)
+    elif Path(args.input).is_file():
+        inputdata = (args.input, args.output)
+        final_result, tot_samples = get_results(inputdata)
     return final_result, tot_samples
 
 if __name__ == '__main__':
     start_time = transcriber.get_time()
-    tot_samples = main(args.model, args.input, args.output, args.outputtype, args.list_models, args.model_name, args.lang, args.log)[1]
+    tot_samples = main(args)[1]
     diff_end_start, sec, mcsec = transcriber.send_time(start_time)
-    print(f'''Script info: execution time: {sec} sec, {mcsec} mcsec; xRT: {format(tot_samples / 16000.0 / float(diff_end_start), '.3f')}''')
+    logging.info(f'''Script info: execution time: {sec} sec, {mcsec} mcsec; xRT: {format(tot_samples / 16000.0 / float(diff_end_start), '.3f')}''')
