3f8ec0a71e0b700a26ca86aced66e60f3ed92db2
Vadim Doroshenko <vdk231190@mail.ru>
2022-04-18 17:46:59 +0200

    OOP correction; changed log msgs; added log msg if I/O paths do not exist

--- python/transcriber/transcriber.py
+++ python/transcriber/transcriber.py
@@ -10,7 +10,6 @@ import os
 import re
 import logging
 
-from datetime import datetime as dt
 from vosk import KaldiRecognizer, Model
 from pathlib import Path
 
@@ -21,7 +20,7 @@ MODEL_LIST_URL = MODEL_PRE_PATH + 'model-list.json'
 
 class Transcriber:
 
-    def transcribe(model, process, args):
+    def transcribe(self, model, process, args):
         rec = KaldiRecognizer(model, 16000)
         rec.SetWords(True)
 
@@ -58,7 +57,7 @@ class Transcriber:
                 final_result += part['text'] + ' '
         return final_result, tot_samples
 
-    def resample_ffmpeg(infile):
+    def resample_ffmpeg(self, infile):
         stream = subprocess.Popen(
             ['ffmpeg', '-nostdin', '-loglevel', 'quiet', '-i', 
             infile, 
@@ -66,22 +65,12 @@ class Transcriber:
             stdout=subprocess.PIPE)
         return stream
 
-    def get_start_time():
-        start_time = dt.now()
-        return start_time
-
-    def get_end_time(start_time):
-        script_time = str(dt.now() - start_time)
-        seconds = script_time[5:8].strip('0')
-        mcseconds = script_time[8:].strip('0')
-        return script_time.strip(':0'), seconds.rstrip('.'), mcseconds
-
-    def get_file_list(args):
+    def get_file_list(self, args):
         arg_list = []
         [arg_list.append((Path(args.input, files), Path(args.output, Path(files).stem).with_suffix('.' + args.outputtype))) for files in os.listdir(args.input)]
         return arg_list
 
-    def check_args(args):
+    def check_args(self, args):
     
         def models_list():
             response = requests.get(MODEL_LIST_URL)
@@ -100,54 +89,52 @@ class Transcriber:
         elif args.languages_list == True:
             languages_list()
 
-    def get_model(args):
-
-        def get_model_name_using_model_name_arg(args, models_path):
-            if not Path.is_dir(Path(models_path, args.model_name)):
-                response = requests.get(MODEL_LIST_URL)
-                result_model = [model['name'] for model in response.json() if model['name'] == args.model_name]
-                if result_model == []:
-                    logging.info('model name "%s" does not exist, set -models_list to see available models' % (args.model_name))
-                    exit(1)
-                else:
-                    result_model = result_model[0]
+    def get_model_name_using_model_name_arg(self, args, models_path):
+        if not Path.is_dir(Path(models_path, args.model_name)):
+            response = requests.get(MODEL_LIST_URL)
+            result_model = [model['name'] for model in response.json() if model['name'] == args.model_name]
+            if result_model == []:
+                logging.info('model name "%s" does not exist, set -models_list to see available models' % (args.model_name))
+                exit(1)
             else:
-                result_model = args.model_name
-            return result_model
+                result_model = result_model[0]
+        else:
+            result_model = args.model_name
+        return result_model
 
-
-        def get_model_name_using_lang_arg(args, models_path):
-            model_file_list = os.listdir(models_path)
-            model_file = [model for model in model_file_list if re.match(f"vosk-model(-small)?-{args.lang}", model)]
-            if model_file == []:
-                response = requests.get(MODEL_LIST_URL)
-                result_model = [model['name'] for model in response.json() if model['lang'] == args.lang and model['type'] == 'small' and model['obsolete'] == 'false']
-                if result_model == []:
-                    logging.info('language "%s" does not exist, set -languages_list to see available languages' % (args.lang))
-                    exit(1)
-                else:
-                    result_model = result_model[0]
+    def get_model_name_using_lang_arg(self, args, models_path):
+        model_file_list = os.listdir(models_path)
+        model_file = [model for model in model_file_list if re.match(f"vosk-model(-small)?-{args.lang}", model)]
+        if model_file == []:
+            response = requests.get(MODEL_LIST_URL)
+            result_model = [model['name'] for model in response.json() if model['lang'] == args.lang and model['type'] == 'small' and model['obsolete'] == 'false']
+            if result_model == []:
+                logging.info('language "%s" does not exist, set -languages_list to see available languages' % (args.lang))
+                exit(1)
             else:
-                result_model = model_file[0]
-            return result_model
+                result_model = result_model[0]
+        else:
+            result_model = model_file[0]
+        return result_model
         
-        def download_model(model_location, models_path):
-            if not Path(model_location).exists():
-                model_zip = model_location + '.zip'
-                urllib.request.urlretrieve(MODEL_PRE_PATH + model_location[len(str(Path(model_location).parent))+1:] + '.zip', model_zip)
-                with zipfile.ZipFile(model_zip, 'r') as model_ref:
-                    model_ref.extractall(models_path)
-                Path.unlink(Path(model_zip))
-            model = Model(model_location)
-            return model
+    def download_model(self, model_location, models_path):
+        if not Path(model_location).exists():
+            model_zip = model_location + '.zip'
+            urllib.request.urlretrieve(MODEL_PRE_PATH + model_location[len(str(Path(model_location).parent))+1:] + '.zip', model_zip)
+            with zipfile.ZipFile(model_zip, 'r') as model_ref:
+                model_ref.extractall(models_path)
+            Path.unlink(Path(model_zip))
+        model = Model(model_location)
+        return model
 
+    def get_model(self, args):    
         models_path = Path.home() / '.cache' / 'vosk'
         if not Path.is_dir(models_path):
             Path.mkdir(models_path)
         if args.lang == None:
-            result_model = get_model_name_using_model_name_arg(args, models_path)
+            result_model = self.get_model_name_using_model_name_arg(args, models_path)
         else:
-            result_model = get_model_name_using_lang_arg(args, models_path)
+            result_model = self.get_model_name_using_lang_arg(args, models_path)
         model_location = str(Path(models_path, result_model))
-        model = download_model(model_location, models_path)
+        model = self.download_model(model_location, models_path)
         return model
--- python/transcriber/vosk_transcriber.py
+++ python/transcriber/vosk_transcriber.py
@@ -3,7 +3,8 @@
 import logging
 import argparse
 
-from transcriber import Transcriber as transcriber
+from datetime import datetime as dt
+from transcriber import Transcriber
 from multiprocessing.dummy import Pool
 from pathlib import Path
 
@@ -60,6 +61,8 @@ def get_results(inputdata):
 
 def main(args):
     global model
+    global transcriber
+    transcriber = Transcriber()
     transcriber.check_args(args)
     if args.input:
         model = transcriber.get_model(args)
@@ -68,11 +71,11 @@ def main(args):
             with Pool() as pool:
                 for final_result, tot_samples in pool.map(get_results, file_list):
                     return final_result, tot_samples
-        elif Path(args.input).is_file() and Path(args.output).is_file() or args.output == '':
+        elif Path(args.input).is_file():
             inputdata = (args.input, args.output)
             final_result, tot_samples = get_results(inputdata)
-        else:
-            logging.info('Please specify input and output arguments as files or folders simultaneously')
+        elif not Path(args.input).exists() or not Path(args.output).exists():
+            logging.info('Please set correct input/output paths')
             exit(1)
         return final_result, tot_samples
     else:
@@ -80,7 +83,18 @@ def main(args):
         exit(1)
 
 if __name__ == '__main__':
-    start_time = transcriber.get_start_time()
+
+    def get_start_time():
+        start_time = dt.now()
+        return start_time
+
+    def get_end_time(start_time):
+        script_time = str(dt.now() - start_time)
+        seconds = script_time[5:8].strip('0')
+        mcseconds = script_time[8:].strip('0')
+        return script_time.strip(':0'), seconds.rstrip('.'), mcseconds
+
+    start_time = get_start_time()
     tot_samples = main(args)[1]
-    diff_end_start, sec, mcsec = transcriber.get_end_time(start_time)
+    diff_end_start, sec, mcsec = get_end_time(start_time)
     logging.info(f'''Execution time: {sec} sec, {mcsec} mcsec; xRT: {format(tot_samples / 16000.0 / float(diff_end_start), '.3f')}''')
