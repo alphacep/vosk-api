97d737a30afe7cbe4a67b36acb4e7831aad3f361
Nickolay Shmyrev <nshmyrev@gmail.com>
2022-05-15 23:36:46 +0200

    Bump version

--- python/setup.py
+++ python/setup.py
@@ -44,7 +44,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vosk",
-    version="0.3.37",
+    version="0.3.38",
     author="Alpha Cephei Inc",
     author_email="contact@alphacephei.com",
     description="Offline open source speech recognition API based on Kaldi and Vosk",
