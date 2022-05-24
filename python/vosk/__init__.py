d0f0315518fe507dcfd1ca78131f34887a2f9023
Vadim Doroshenko <vdk231190@mail.ru>
2022-05-24 16:24:32 +0200

    changed download_model methond; added my_hook method

--- python/vosk/__init__.py
+++ python/vosk/__init__.py
@@ -89,21 +89,25 @@ class Model(object):
     def download_model(self, model_name):
         if not MODEL_DIRS[3].exists():
             MODEL_DIRS[3].mkdir()
-        with TqdmUpTo(unit='B', unit_scale=True, unit_divisor=1024, miniters=1,
+        with tqdm(unit='B', unit_scale=True, unit_divisor=1024, miniters=1,
                 desc=(MODEL_PRE_URL + str(model_name.name) + '.zip').split('/')[-1]) as t:
+            reporthook = self.my_hook(t)
+            urlretrieve(MODEL_PRE_URL + str(model_name.name) + '.zip', str(model_name) + '.zip', 
+                reporthook=reporthook, data=None)
+            t.total = t.n
+            with ZipFile(str(model_name) + '.zip', 'r') as model_ref:
+                model_ref.extractall(model_name.parent)
+            Path(str(model_name) + '.zip').unlink()
 
-                urlretrieve(MODEL_PRE_URL + str(model_name.name) + '.zip', str(model_name) + '.zip', reporthook=t.update_to, data=None)
-                t.total = t.n
-                with ZipFile(str(model_name) + '.zip', 'r') as model_ref:
-                    model_ref.extractall(model_name.parent)
-                Path(str(model_name) + '.zip').unlink()
-
-class TqdmUpTo(tqdm):
-
-    def update_to(self, b=1, bsize=1, tsize=None):
-        if tsize is not None:
-            self.total = tsize
-        return self.update(b * bsize - self.n)       
+    def my_hook(self, t):
+        last_b = [0]
+        def update_to(b=1, bsize=1, tsize=None):
+            if tsize not in (None, -1):
+                t.total = tsize
+            displayed = t.update((b - last_b[0]) * bsize)
+            last_b[0] = b
+            return displayed
+        return update_to
 
 class SpkModel(object):
 
