b8a88cc30c58ed299e7d4dbf36cb847685e657bd
vadimdddd <68805308+vadimdddd@users.noreply.github.com>
2022-08-19 17:31:47 +0300

    Add gradio recognizer (#1114)
    Demonstrate speech recognition in gradio

---
+++ python/example/test_gradio.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from vosk import KaldiRecognizer, Model
+import gradio as gr
+import json
+
+model = Model(lang="en-us")
+
+def transcribe(data, state):
+    sample_rate, audio_data = data
+    audio_data = (audio_data >> 16).astype("int16").tobytes()
+    
+    if state is None:
+        rec = KaldiRecognizer(model, sample_rate)
+        result = []
+    else:
+        rec, result = state
+    
+    if rec.AcceptWaveform(audio_data):
+        text_result = json.loads(rec.Result())["text"]
+        if text_result != '':
+            result.append(text_result)
+        partial_result = ''
+    else:
+        partial_result = json.loads(rec.PartialResult())["partial"] + " "
+
+    return '\n'.join(result) + '\n' + partial_result, (rec, result)
+
+gr.Interface(
+    fn=transcribe,
+    inputs=[
+        gr.Audio(source="microphone", type="numpy", streaming=True),
+        "state"
+    ],
+    outputs=[
+        "textbox",
+        "state"
+    ],
+    live=True).launch(share=True)
