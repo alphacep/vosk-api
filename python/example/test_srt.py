2376b32a8ae6e4ce5f553791fec996b418290aec
vadimdddd <68805308+vadimdddd@users.noreply.github.com>
2022-08-24 12:48:18 +0300

    Add SRTResult to KaldiRecognizer (#1080)

--- python/example/test_srt.py
+++ python/example/test_srt.py
@@ -2,12 +2,7 @@
 
 from vosk import Model, KaldiRecognizer, SetLogLevel
 import sys
-import os
-import wave
 import subprocess
-import srt
-import json
-import datetime
 
 SetLogLevel(-1)
 
@@ -16,37 +11,9 @@ model = Model(lang="en-us")
 rec = KaldiRecognizer(model, sample_rate)
 rec.SetWords(True)
 
-process = subprocess.Popen(['ffmpeg', '-loglevel', 'quiet', '-i',
+stream = subprocess.Popen(['ffmpeg', '-loglevel', 'quiet', '-i',
                             sys.argv[1],
                             '-ar', str(sample_rate) , '-ac', '1', '-f', 's16le', '-'],
-                            stdout=subprocess.PIPE)
+                            stdout=subprocess.PIPE).stdout
 
-
-WORDS_PER_LINE = 7
-
-def transcribe():
-    results = []
-    subs = []
-    while True:
-       data = process.stdout.read(4000)
-       if len(data) == 0:
-           break
-       if rec.AcceptWaveform(data):
-           results.append(rec.Result())
-    results.append(rec.FinalResult())
-
-    for i, res in enumerate(results):
-       jres = json.loads(res)
-       if not 'result' in jres:
-           continue
-       words = jres['result']
-       for j in range(0, len(words), WORDS_PER_LINE):
-           line = words[j : j + WORDS_PER_LINE] 
-           s = srt.Subtitle(index=len(subs), 
-                   content=" ".join([l['word'] for l in line]),
-                   start=datetime.timedelta(seconds=line[0]['start']), 
-                   end=datetime.timedelta(seconds=line[-1]['end']))
-           subs.append(s)
-    return subs
-
-print (srt.compose(transcribe()))
+print(rec.SrtResult(stream))
