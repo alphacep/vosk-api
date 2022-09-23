c8774f27561fdf95ff19442aa3100a640350f9c4
Vadim Doroshenko <vdk231190@mail.ru>
2022-09-23 14:42:30 +0200

    updated algorithm

--- python/example/test_big_file.py
+++ python/example/test_big_file.py
@@ -5,9 +5,12 @@ import os
 import re
 import subprocess
 import json
+import shlex
 
 from pathlib import Path
 from vosk import Model, KaldiRecognizer, SetLogLevel
+from multiprocessing.dummy import Pool
+from queue import Queue
 
 SetLogLevel(0)
 
@@ -25,11 +28,21 @@ parser.add_argument(
 parser.add_argument(
         "--input", "-i", type=str,
         help="audiofile")
+parser.add_argument(
+        "--cores", "-c", default=4, type=int,
+        help="PC cores used for recognize")
 
 class HugeFileProcessor:
 
     def __init__(self, args):
         self.args = args
+        self.queue = Queue()
+
+    def resample_ffmpeg(self, infile):
+        cmd = shlex.split("ffmpeg -nostdin -loglevel quiet "
+                "-i \'{}\' -ar {} -ac 1 -f s16le -".format(str(infile), SAMPLE_RATE))
+        stream = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        return stream
 
     def get_model(self):
         for directory in MODEL_DIRS:
@@ -41,71 +54,72 @@ class HugeFileProcessor:
             if len(model_files) == 2:
                 return Path(directory, model_files[0]), Path(directory, model_files[1])
 
-    def process_by_small_model(self, small_model_path, args):
+    def process_by_small_model(self, small_model_path):
 
         model = Model(model_path=str(small_model_path))
         rec = KaldiRecognizer(model, SAMPLE_RATE)
         rec.SetPartialWords(True)
         fragments = []
 
-        with subprocess.Popen(["ffmpeg", "-loglevel", "quiet", "-i",
-            args.input,
-            "-ar", str(SAMPLE_RATE) , "-ac", "1", "-f", "s16le", "-"],
-            stdout=subprocess.PIPE) as stream:
+        stream = self.resample_ffmpeg(self.args.input)
 
-            while True:
-                data = stream.stdout.read(4000)
-                if len(data) == 0:
-                    break
-                if rec.AcceptWaveform(data):
-                    rec.Result()
-                    if part_res["partial"] != '' and part_res["partial_result"] != '':
-                        fragments.append([int(part_res["partial_result"][0]["start"]),
-                                int(part_res["partial_result"][-1]["end"])])
-                else:
-                    part_res = json.loads(rec.PartialResult())
-            rec.FinalResult()
+        while True:
+            data = stream.stdout.read(4000)
+            if len(data) == 0:
+                break
+            if rec.AcceptWaveform(data):
+                rec.Result()
+                if part_res["partial"] != '' and part_res["partial_result"] != '':
+                    fragments.append((int(part_res["partial_result"][0]["start"]),
+                            int(part_res["partial_result"][-1]["end"])))
+            else:
+                part_res = json.loads(rec.PartialResult())
+        rec.FinalResult()
         return fragments
 
-    def process_by_big_model(self, item, big_model_path, args):
+    def process_by_big_model(self, timestamps):
+
+        item_0, item_1 = self.queue.get_nowait()
 
         model = Model(model_path=str(big_model_path))
         rec = KaldiRecognizer(model, SAMPLE_RATE)
-        start_pos = 32000 * item[0]
-        end_pos = 32000 * item[1]
+        start_pos = 32000 * item_0
+        end_pos = 32000 * item_1
 
-        with subprocess.Popen(["ffmpeg", "-loglevel", "quiet", "-i",
-            args.input,
-            "-ar", str(SAMPLE_RATE) , "-ac", "1", "-f", "s16le", "-"],
-            stdout=subprocess.PIPE) as stream:
+        stream = self.resample_ffmpeg(self.args.input)
+        stream.stdout.read(start_pos)
 
-            stream.stdout.read(start_pos)
-
-            while True:
-                data = stream.stdout.read(4000)
-                if end_pos - start_pos == 0:
-                    break
-                if rec.AcceptWaveform(data):
-                    rec.Result()
-                else:
-                    print(rec.PartialResult())
-                start_pos += 4000
-            rec.FinalResult()
+        while True:
+            data = stream.stdout.read(4000)
+            if end_pos - start_pos == 0:
+                break
+            if rec.AcceptWaveform(data):
+                rec.Result()
+            else:
+                print(rec.PartialResult())
+            start_pos += 4000
+        print(rec.FinalResult())
         return
 
-    def process(self, small_model_path, big_model_path, args):
+    def process(self, small_model_path):
 
-        frag = self.process_by_small_model(small_model_path, args)
-        for item in frag:
-            self.process_by_big_model(item, big_model_path, args)
+        timestamps = self.process_by_small_model(small_model_path)
+        
+        for x in timestamps:
+            self.queue.put(x)
+
+        with Pool(self.args.cores) as pool:
+            pool.map(self.process_by_big_model, timestamps)
 
 def main():
 
     args = parser.parse_args()
+
     processor = HugeFileProcessor(args)
+    global big_model_path
     small_model_path, big_model_path = processor.get_model()
 
-    processor.process(small_model_path, big_model_path, args)
+    processor.process(small_model_path)
 
 if __name__ == "__main__":
     main()
