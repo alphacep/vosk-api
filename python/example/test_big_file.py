c432667d57937cc721260d6ee5175ed5e77d48f8
Vadim Doroshenko <vdk231190@mail.ru>
2022-10-20 13:58:48 +0200

    changed model selection from language to model name

--- python/example/test_big_file.py
+++ python/example/test_big_file.py
@@ -15,7 +15,7 @@ from pathlib import Path
 from tqdm import tqdm
 from urllib.request import urlretrieve
 from zipfile import ZipFile
-from re import match
+from re import split
 from vosk import Model, KaldiRecognizer, SetLogLevel, list_languages
 from multiprocessing.dummy import Pool
 from timeit import default_timer as timer
@@ -31,8 +31,11 @@ MODEL_DIRS = [os.getenv("VOSK_MODEL_PATH"), Path("usr/share/vosk"),
 parser = argparse.ArgumentParser(
         description = "Transcribe big size audiofiles")
 parser.add_argument(
-        "--lang", "-l", default="en-us", type=str,
-        help="select both model language")
+        "--small", "-s", default="vosk-model-small-en-us-0.15", type=str,
+        help="small model name")
+parser.add_argument(
+        "--big", "-b", default="vosk-model-en-us-0.22", type=str,
+        help="big model name")
 parser.add_argument(
         "--input", "-i", type=str,
         help="audiofile")
@@ -46,65 +49,12 @@ parser.add_argument(
         "--list-languages", default=False, action="store_true",
         help="list available languages")
 
-def download_progress_hook(t):
-    last_b = [0]
-    def update_to(b=1, bsize=1, tsize=None):
-        if tsize not in (None, -1):
-            t.total = tsize
-        displayed = t.update((b - last_b[0]) * bsize)
-        last_b[0] = b
-        return displayed
-    return update_to
-
-def download_model(small_model_path, big_model_path):
-    if not (small_model_path.parent).exists():
-        (small_model_path.parent).mkdir(parents=True)
-    model_names = [small_model_path, big_model_path]
-    for model_name in model_names:
-        if model_name.exists():
-            continue
-        with tqdm(unit="B", unit_scale=True, unit_divisor=1024, miniters=1,
-                desc=(MODEL_PRE_URL + str(model_name.name) + ".zip").rsplit("/",
-                    maxsplit=1)[-1]) as t:
-            reporthook = download_progress_hook(t)
-            urlretrieve(MODEL_PRE_URL + str(model_name.name) + ".zip",
-                    str(model_name) + ".zip", reporthook=reporthook, data=None)
-            t.total = t.n
-            with ZipFile(str(model_name) + ".zip", "r") as model_ref:
-                model_ref.extractall(model_name.parent)
-            Path(str(model_name) + ".zip").unlink()
-
-def get_models(args):
-    for directory in MODEL_DIRS:
-        if directory is None or not Path(directory).exists():
-            continue
-        model_file_list = os.listdir(directory)
-        small_model_file = [model for model in model_file_list if
-                match(r"vosk-model-small-{}".format(args.lang), model)]
-        big_model_file = [model for model in model_file_list if
-                match(r"vosk-model-{}".format(args.lang), model)]
-        if small_model_file != [] and big_model_file != []:
-            return Path(directory, small_model_file[0]), Path(directory, big_model_file[0])
-    response = requests.get(MODEL_LIST_URL, timeout=10)
-    result_small_model = [model["name"] for model in response.json() if
-            model["lang"] == args.lang and model["type"] == "small" and
-            model["obsolete"] == "false"]
-    result_big_model = [model["name"] for model in response.json() if
-            model["lang"] == args.lang and model["type"] == "big" and
-            model["obsolete"] == "false"]
-    if result_small_model == [] or result_big_model == []:
-        print("lang %s does not exist" % (args.lang))
-        sys.exit(1)
-    else:
-        download_model(Path(directory, result_small_model[0]), Path(directory, result_big_model[0]))
-        return Path(directory, result_small_model[0]), Path(directory, result_big_model[0])
-
 class BigFileProcessor:
 
-    def __init__(self, args, small_model_path, big_model_path):
+    def __init__(self, args):
         self.args = args
-        self.small_model = Model(model_path=str(small_model_path))
-        self.big_model = Model(model_path=str(big_model_path))
+        self.small_model = Model(model_name=str(args.small))
+        self.big_model = Model(model_name=str(args.big))
 
     def resample_ffmpeg(self):
         cmd = shlex.split("ffmpeg -nostdin -loglevel quiet "
@@ -157,14 +107,14 @@ class BigFileProcessor:
                             start=datetime.timedelta(seconds=line[0]["start"]),
                             end=datetime.timedelta(seconds=line[-1]["end"]))
                     subs.append(s)
-            result.append(srt.compose(subs))
+            return srt.compose(subs)
         else:
             return json.loads(results[0])["text"]
-        return result[0]
 
     def process_file(self):
         fragments = self.prepare_fragments()
         logging.info("File fragments are ready")
+        
         with Pool() as pool:
             for fragment in pool.map(self.format_result, fragments):
                 print(fragment)
@@ -178,7 +128,21 @@ def main():
     if args.list_languages is True:
         list_languages()
         return
-    
+
+    if not args.input:
+        logging.info("Please specify input file")
+        sys.exit(1)
+
+    small_model_lang = split(r"(-\d.+)", split(r"vosk-model-(small-)*", args.small)[-1])[0]
+    big_model_lang = split(r"(-\d.+)", split(r"vosk-model-(small-)*", args.big)[-1])[0]
+    if not small_model_lang == big_model_lang:
+        logging.info("You have to use both models for the same language, try again.")
+        sys.exit(1)
+
+    if args.list_languages is True:
+        list_languages()
+        return
+
     if not args.input:
         logging.info("Please specify input file")
         sys.exit(1)
@@ -188,10 +152,7 @@ def main():
         "please try again.")
         sys.exit(1)
 
-    small_model_path, big_model_path = get_models(args)
-    logging.info("Models are ready")
-
-    processor = BigFileProcessor(args, small_model_path, big_model_path)
+    processor = BigFileProcessor(args)
 
     start_time = timer()
 
