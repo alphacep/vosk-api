4c10e58dd07f95e6671fc2b37b1fa49f0a4a3d3f
Tomas Mehdi <tomymehdi@gmail.com>
2022-10-26 21:34:57 +0200

    JSON output_type with texts

--- python/vosk/transcriber/transcriber.py
+++ python/vosk/transcriber/transcriber.py
@@ -96,7 +96,10 @@ class Transcriber:
                     processed_result += part["text"] + "\n"
 
         elif self.args.output_type == "json":
-            monologues = {"schemaVersion":"2.0", "monologues":[]}
+            monologues = {"schemaVersion":"2.0", "monologues":[], "text":[]}
+            for part in result:
+                if part["text"] != "":
+                    monologue["text"] += part["text"]
             for _, res in enumerate(result):
                 if not "result" in res:
                     continue
