789009dd35ba1264d910d5c85d48953c45c3b30b
doomsdayrs <doomsdayrs@gmail.com>
2022-12-26 16:48:49 -0500

    Upgrade android tools to 7.3.0

--- kotlin/settings.gradle.kts
+++ kotlin/settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             if (requested.id.namespace == "com.android") {
-                useModule("com.android.tools.build:gradle:7.0.4")
+                useModule("com.android.tools.build:gradle:7.3.0")
             }
         }
     }
