7c25bf2f720b939442d23d50f42ff8a4b74a7ae4
Martin Mende <martin.mende@aristech.de>
2023-05-19 08:57:33 +0200

    Added vosk_recognizer_set_grm_with_lexicon

--- src/model.h
+++ src/model.h
@@ -18,19 +18,19 @@
 #include "base/kaldi-common.h"
 #include "fstext/fstext-lib.h"
 #include "fstext/fstext-utils.h"
-#include "online2/onlinebin-util.h"
-#include "online2/online-timing.h"
-#include "online2/online-endpoint.h"
-#include "online2/online-nnet3-incremental-decoding.h"
-#include "online2/online-feature-pipeline.h"
 #include "lat/lattice-functions.h"
 #include "lat/sausages.h"
 #include "lat/word-align-lattice.h"
 #include "lm/const-arpa-lm.h"
-#include "util/parse-options.h"
 #include "nnet3/nnet-utils.h"
-#include "rnnlm/rnnlm-utils.h"
+#include "online2/online-endpoint.h"
+#include "online2/online-feature-pipeline.h"
+#include "online2/online-nnet3-incremental-decoding.h"
+#include "online2/online-timing.h"
+#include "online2/onlinebin-util.h"
 #include "rnnlm/rnnlm-lattice-rescoring.h"
+#include "rnnlm/rnnlm-utils.h"
+#include "util/parse-options.h"
 #include <atomic>
 
 using namespace kaldi;
@@ -41,66 +41,71 @@ class Recognizer;
 class Model {
 
 public:
-    Model(const char *model_path);
-    void Ref();
-    void Unref();
-    int FindWord(const char *word);
+  Model(const char *model_path);
+  void Ref();
+  void Unref();
+  int FindWord(const char *word);
 
 protected:
-    ~Model();
-    void ConfigureV1();
-    void ConfigureV2();
-    void ReadDataFiles();
+  ~Model();
+  void ConfigureV1();
+  void ConfigureV2();
+  void ReadDataFiles();
 
-    friend class Recognizer;
+  friend class Recognizer;
 
-    string model_path_str_;
-    string nnet3_rxfilename_;
-    string hclg_fst_rxfilename_;
-    string hcl_fst_rxfilename_;
-    string g_fst_rxfilename_;
-    string disambig_rxfilename_;
-    string word_syms_rxfilename_;
-    string winfo_rxfilename_;
-    string carpa_rxfilename_;
-    string std_fst_rxfilename_;
-    string final_ie_rxfilename_;
-    string mfcc_conf_rxfilename_;
-    string fbank_conf_rxfilename_;
-    string global_cmvn_stats_rxfilename_;
-    string pitch_conf_rxfilename_;
+  string model_path_str_;
+  string nnet3_rxfilename_;
+  string hclg_fst_rxfilename_;
+  string hcl_fst_rxfilename_;
+  string g_fst_rxfilename_;
+  string ctx_dep_rxfilename_;
+  string disambig_rxfilename_;
+  string word_syms_rxfilename_;
+  string winfo_rxfilename_;
+  string carpa_rxfilename_;
+  string std_fst_rxfilename_;
+  string final_ie_rxfilename_;
+  string mfcc_conf_rxfilename_;
+  string fbank_conf_rxfilename_;
+  string global_cmvn_stats_rxfilename_;
+  string pitch_conf_rxfilename_;
+  string phone_syms_rxfilename_;
 
-    string rnnlm_word_feats_rxfilename_;
-    string rnnlm_feat_embedding_rxfilename_;
-    string rnnlm_config_rxfilename_;
-    string rnnlm_lm_rxfilename_;
+  string rnnlm_word_feats_rxfilename_;
+  string rnnlm_feat_embedding_rxfilename_;
+  string rnnlm_config_rxfilename_;
+  string rnnlm_lm_rxfilename_;
 
-    kaldi::OnlineEndpointConfig endpoint_config_;
-    kaldi::LatticeIncrementalDecoderConfig nnet3_decoding_config_;
-    kaldi::nnet3::NnetSimpleLoopedComputationOptions decodable_opts_;
-    kaldi::OnlineNnet2FeaturePipelineInfo feature_info_;
+  kaldi::OnlineEndpointConfig endpoint_config_;
+  kaldi::LatticeIncrementalDecoderConfig nnet3_decoding_config_;
+  kaldi::nnet3::NnetSimpleLoopedComputationOptions decodable_opts_;
+  kaldi::OnlineNnet2FeaturePipelineInfo feature_info_;
 
-    kaldi::nnet3::DecodableNnetSimpleLoopedInfo *decodable_info_ = nullptr;
-    kaldi::TransitionModel *trans_model_ = nullptr;
-    kaldi::nnet3::AmNnetSimple *nnet_ = nullptr;
-    const fst::SymbolTable *word_syms_ = nullptr;
-    bool word_syms_loaded_ = false;
-    kaldi::WordBoundaryInfo *winfo_ = nullptr;
-    vector<int32> disambig_;
+  kaldi::nnet3::DecodableNnetSimpleLoopedInfo *decodable_info_ = nullptr;
+  kaldi::TransitionModel *trans_model_ = nullptr;
+  kaldi::nnet3::AmNnetSimple *nnet_ = nullptr;
+  const fst::SymbolTable *word_syms_ = nullptr;
+  bool word_syms_loaded_ = false;
+  kaldi::WordBoundaryInfo *winfo_ = nullptr;
+  vector<int32> disambig_;
+  const fst::SymbolTable *phone_syms_ = nullptr;
 
-    fst::Fst<fst::StdArc> *hclg_fst_ = nullptr;
-    fst::Fst<fst::StdArc> *hcl_fst_ = nullptr;
-    fst::Fst<fst::StdArc> *g_fst_ = nullptr;
+  fst::Fst<fst::StdArc> *hclg_fst_ = nullptr;
+  fst::Fst<fst::StdArc> *hcl_fst_ = nullptr;
+  fst::Fst<fst::StdArc> *g_fst_ = nullptr;
 
-    fst::VectorFst<fst::StdArc> *graph_lm_fst_ = nullptr;
-    kaldi::ConstArpaLm const_arpa_;
+  ContextDependency *ctx_dep_ = nullptr;
 
-    kaldi::rnnlm::RnnlmComputeStateComputationOptions rnnlm_compute_opts;
-    CuMatrix<BaseFloat> word_embedding_mat;
-    kaldi::nnet3::Nnet rnnlm;
-    bool rnnlm_enabled_ = false;
+  fst::VectorFst<fst::StdArc> *graph_lm_fst_ = nullptr;
+  kaldi::ConstArpaLm const_arpa_;
 
-    std::atomic<int> ref_cnt_;
+  kaldi::rnnlm::RnnlmComputeStateComputationOptions rnnlm_compute_opts;
+  CuMatrix<BaseFloat> word_embedding_mat;
+  kaldi::nnet3::Nnet rnnlm;
+  bool rnnlm_enabled_ = false;
+
+  std::atomic<int> ref_cnt_;
 };
 
 #endif /* VOSK_MODEL_H */
--- src/vosk_api.cc
+++ src/vosk_api.cc
@@ -14,277 +14,264 @@
 
 #include "vosk_api.h"
 
-#include "recognizer.h"
 #include "model.h"
+#include "recognizer.h"
 #include "spk_model.h"
 
 #if HAVE_CUDA
-#include "cudamatrix/cu-device.h"
 #include "batch_recognizer.h"
+#include "cudamatrix/cu-device.h"
 #endif
 
 #include <string.h>
 
 using namespace kaldi;
 
-VoskModel *vosk_model_new(const char *model_path)
-{
-    try {
-        return (VoskModel *)new Model(model_path);
-    } catch (...) {
-        return nullptr;
-    }
+VoskModel *vosk_model_new(const char *model_path) {
+  try {
+    return (VoskModel *)new Model(model_path);
+  } catch (...) {
+    return nullptr;
+  }
 }
 
-void vosk_model_free(VoskModel *model)
-{
-    if (model == nullptr) {
-       return;
-    }
-    ((Model *)model)->Unref();
+void vosk_model_free(VoskModel *model) {
+  if (model == nullptr) {
+    return;
+  }
+  ((Model *)model)->Unref();
 }
 
-int vosk_model_find_word(VoskModel *model, const char *word)
-{
-    return (int) ((Model *)model)->FindWord(word);
+int vosk_model_find_word(VoskModel *model, const char *word) {
+  return (int)((Model *)model)->FindWord(word);
 }
 
-VoskSpkModel *vosk_spk_model_new(const char *model_path)
-{
-    try {
-        return (VoskSpkModel *)new SpkModel(model_path);
-    } catch (...) {
-        return nullptr;
-    }
+VoskSpkModel *vosk_spk_model_new(const char *model_path) {
+  try {
+    return (VoskSpkModel *)new SpkModel(model_path);
+  } catch (...) {
+    return nullptr;
+  }
 }
 
-void vosk_spk_model_free(VoskSpkModel *model)
-{
-    if (model == nullptr) {
-       return;
-    }
-    ((SpkModel *)model)->Unref();
+void vosk_spk_model_free(VoskSpkModel *model) {
+  if (model == nullptr) {
+    return;
+  }
+  ((SpkModel *)model)->Unref();
 }
 
-VoskRecognizer *vosk_recognizer_new(VoskModel *model, float sample_rate)
-{
-    try {
-        return (VoskRecognizer *)new Recognizer((Model *)model, sample_rate);
-    } catch (...) {
-        return nullptr;
-    }
+VoskRecognizer *vosk_recognizer_new(VoskModel *model, float sample_rate) {
+  try {
+    return (VoskRecognizer *)new Recognizer((Model *)model, sample_rate);
+  } catch (...) {
+    return nullptr;
+  }
 }
 
-VoskRecognizer *vosk_recognizer_new_spk(VoskModel *model, float sample_rate, VoskSpkModel *spk_model)
-{
-    try {
-        return (VoskRecognizer *)new Recognizer((Model *)model, sample_rate, (SpkModel *)spk_model);
-    } catch (...) {
-        return nullptr;
-    }
+VoskRecognizer *vosk_recognizer_new_spk(VoskModel *model, float sample_rate,
+                                        VoskSpkModel *spk_model) {
+  try {
+    return (VoskRecognizer *)new Recognizer((Model *)model, sample_rate,
+                                            (SpkModel *)spk_model);
+  } catch (...) {
+    return nullptr;
+  }
 }
 
-VoskRecognizer *vosk_recognizer_new_grm(VoskModel *model, float sample_rate, const char *grammar)
-{
-    try {
-        return (VoskRecognizer *)new Recognizer((Model *)model, sample_rate, grammar);
-    } catch (...) {
-        return nullptr;
-    }
+VoskRecognizer *vosk_recognizer_new_grm(VoskModel *model, float sample_rate,
+                                        const char *grammar) {
+  try {
+    return (VoskRecognizer *)new Recognizer((Model *)model, sample_rate,
+                                            grammar);
+  } catch (...) {
+    return nullptr;
+  }
 }
 
-void vosk_recognizer_set_max_alternatives(VoskRecognizer *recognizer, int max_alternatives)
-{
-    ((Recognizer *)recognizer)->SetMaxAlternatives(max_alternatives);
+void vosk_recognizer_set_max_alternatives(VoskRecognizer *recognizer,
+                                          int max_alternatives) {
+  ((Recognizer *)recognizer)->SetMaxAlternatives(max_alternatives);
 }
 
-void vosk_recognizer_set_words(VoskRecognizer *recognizer, int words)
-{
-    ((Recognizer *)recognizer)->SetWords((bool)words);
+void vosk_recognizer_set_words(VoskRecognizer *recognizer, int words) {
+  ((Recognizer *)recognizer)->SetWords((bool)words);
 }
 
-void vosk_recognizer_set_partial_words(VoskRecognizer *recognizer, int partial_words)
-{
-    ((Recognizer *)recognizer)->SetPartialWords((bool)partial_words);
+void vosk_recognizer_set_partial_words(VoskRecognizer *recognizer,
+                                       int partial_words) {
+  ((Recognizer *)recognizer)->SetPartialWords((bool)partial_words);
 }
 
-void vosk_recognizer_set_nlsml(VoskRecognizer *recognizer, int nlsml)
-{
-    ((Recognizer *)recognizer)->SetNLSML((bool)nlsml);
+void vosk_recognizer_set_nlsml(VoskRecognizer *recognizer, int nlsml) {
+  ((Recognizer *)recognizer)->SetNLSML((bool)nlsml);
 }
 
-void vosk_recognizer_set_spk_model(VoskRecognizer *recognizer, VoskSpkModel *spk_model)
-{
-    if (recognizer == nullptr || spk_model == nullptr) {
-       return;
-    }
-    ((Recognizer *)recognizer)->SetSpkModel((SpkModel *)spk_model);
+void vosk_recognizer_set_spk_model(VoskRecognizer *recognizer,
+                                   VoskSpkModel *spk_model) {
+  if (recognizer == nullptr || spk_model == nullptr) {
+    return;
+  }
+  ((Recognizer *)recognizer)->SetSpkModel((SpkModel *)spk_model);
 }
 
-void vosk_recognizer_set_grm(VoskRecognizer *recognizer, char const *grammar)
-{
-    if (recognizer == nullptr) {
-       return;
-    }
-    ((Recognizer *)recognizer)->SetGrm(grammar);
+void vosk_recognizer_set_grm(VoskRecognizer *recognizer, char const *grammar) {
+  if (recognizer == nullptr) {
+    return;
+  }
+  ((Recognizer *)recognizer)->SetGrm(grammar, nullptr, nullptr, 0);
 }
 
-int vosk_recognizer_accept_waveform(VoskRecognizer *recognizer, const char *data, int length)
-{
-    try {
-        return ((Recognizer *)(recognizer))->AcceptWaveform(data, length);
-    } catch (...) {
-        return -1;
-    }
+void vosk_recognizer_set_grm_with_lexicon(VoskRecognizer *recognizer,
+                                          char const *grammar,
+                                          const char *const *words,
+                                          const char *const *pronunciations,
+                                          int num_words) {
+  if (recognizer == nullptr) {
+    return;
+  }
+  ((Recognizer *)recognizer)->SetGrm(grammar, words, pronunciations, num_words);
 }
 
-int vosk_recognizer_accept_waveform_s(VoskRecognizer *recognizer, const short *data, int length)
-{
-    try {
-        return ((Recognizer *)(recognizer))->AcceptWaveform(data, length);
-    } catch (...) {
-        return -1;
-    }
+int vosk_recognizer_accept_waveform(VoskRecognizer *recognizer,
+                                    const char *data, int length) {
+  try {
+    return ((Recognizer *)(recognizer))->AcceptWaveform(data, length);
+  } catch (...) {
+    return -1;
+  }
 }
 
-int vosk_recognizer_accept_waveform_f(VoskRecognizer *recognizer, const float *data, int length)
-{
-    try {
-        return ((Recognizer *)(recognizer))->AcceptWaveform(data, length);
-    } catch (...) {
-        return -1;
-    }
+int vosk_recognizer_accept_waveform_s(VoskRecognizer *recognizer,
+                                      const short *data, int length) {
+  try {
+    return ((Recognizer *)(recognizer))->AcceptWaveform(data, length);
+  } catch (...) {
+    return -1;
+  }
 }
 
-const char *vosk_recognizer_result(VoskRecognizer *recognizer)
-{
-    return ((Recognizer *)recognizer)->Result();
+int vosk_recognizer_accept_waveform_f(VoskRecognizer *recognizer,
+                                      const float *data, int length) {
+  try {
+    return ((Recognizer *)(recognizer))->AcceptWaveform(data, length);
+  } catch (...) {
+    return -1;
+  }
 }
 
-const char *vosk_recognizer_partial_result(VoskRecognizer *recognizer)
-{
-    return ((Recognizer *)recognizer)->PartialResult();
+const char *vosk_recognizer_result(VoskRecognizer *recognizer) {
+  return ((Recognizer *)recognizer)->Result();
 }
 
-const char *vosk_recognizer_final_result(VoskRecognizer *recognizer)
-{
-    return ((Recognizer *)recognizer)->FinalResult();
+const char *vosk_recognizer_partial_result(VoskRecognizer *recognizer) {
+  return ((Recognizer *)recognizer)->PartialResult();
 }
 
-void vosk_recognizer_reset(VoskRecognizer *recognizer)
-{
-    ((Recognizer *)recognizer)->Reset();
+const char *vosk_recognizer_final_result(VoskRecognizer *recognizer) {
+  return ((Recognizer *)recognizer)->FinalResult();
 }
 
-void vosk_recognizer_free(VoskRecognizer *recognizer)
-{
-    delete (Recognizer *)(recognizer);
+void vosk_recognizer_reset(VoskRecognizer *recognizer) {
+  ((Recognizer *)recognizer)->Reset();
 }
 
-void vosk_set_log_level(int log_level)
-{
-    SetVerboseLevel(log_level);
+void vosk_recognizer_free(VoskRecognizer *recognizer) {
+  delete (Recognizer *)(recognizer);
 }
 
-void vosk_gpu_init()
-{
+void vosk_set_log_level(int log_level) { SetVerboseLevel(log_level); }
+
+void vosk_gpu_init() {
 #if HAVE_CUDA
-//    kaldi::CuDevice::EnableTensorCores(true);
-//    kaldi::CuDevice::EnableTf32Compute(true);
-    kaldi::CuDevice::Instantiate().SelectGpuId("yes");
-    kaldi::CuDevice::Instantiate().AllowMultithreading();
+  //    kaldi::CuDevice::EnableTensorCores(true);
+  //    kaldi::CuDevice::EnableTf32Compute(true);
+  kaldi::CuDevice::Instantiate().SelectGpuId("yes");
+  kaldi::CuDevice::Instantiate().AllowMultithreading();
 #endif
 }
 
-void vosk_gpu_thread_init()
-{
+void vosk_gpu_thread_init() {
 #if HAVE_CUDA
-    kaldi::CuDevice::Instantiate();
+  kaldi::CuDevice::Instantiate();
 #endif
 }
 
-VoskBatchModel *vosk_batch_model_new(const char *model_path)
-{
+VoskBatchModel *vosk_batch_model_new(const char *model_path) {
 #if HAVE_CUDA
-    return (VoskBatchModel *)(new BatchModel(model_path));
+  return (VoskBatchModel *)(new BatchModel(model_path));
 #else
-    return NULL;
+  return NULL;
 #endif
 }
 
-void vosk_batch_model_free(VoskBatchModel *model)
-{
+void vosk_batch_model_free(VoskBatchModel *model) {
 #if HAVE_CUDA
-    delete ((BatchModel *)model);
+  delete ((BatchModel *)model);
 #endif
 }
 
-void vosk_batch_model_wait(VoskBatchModel *model)
-{
+void vosk_batch_model_wait(VoskBatchModel *model) {
 #if HAVE_CUDA
-    ((BatchModel *)model)->WaitForCompletion();
+  ((BatchModel *)model)->WaitForCompletion();
 #endif
 }
 
-VoskBatchRecognizer *vosk_batch_recognizer_new(VoskBatchModel *model, float sample_rate)
-{
+VoskBatchRecognizer *vosk_batch_recognizer_new(VoskBatchModel *model,
+                                               float sample_rate) {
 #if HAVE_CUDA
-    return (VoskBatchRecognizer *)(new BatchRecognizer((BatchModel *)model, sample_rate));
+  return (VoskBatchRecognizer *)(new BatchRecognizer((BatchModel *)model,
+                                                     sample_rate));
 #else
-    return NULL;
+  return NULL;
 #endif
 }
 
-void vosk_batch_recognizer_free(VoskBatchRecognizer *recognizer)
-{
+void vosk_batch_recognizer_free(VoskBatchRecognizer *recognizer) {
 #if HAVE_CUDA
-    delete ((BatchRecognizer *)recognizer);
+  delete ((BatchRecognizer *)recognizer);
 #endif
 }
 
-void vosk_batch_recognizer_accept_waveform(VoskBatchRecognizer *recognizer, const char *data, int length)
-{
+void vosk_batch_recognizer_accept_waveform(VoskBatchRecognizer *recognizer,
+                                           const char *data, int length) {
 #if HAVE_CUDA
-    ((BatchRecognizer *)recognizer)->AcceptWaveform(data, length);
+  ((BatchRecognizer *)recognizer)->AcceptWaveform(data, length);
 #endif
 }
 
-void vosk_batch_recognizer_set_nlsml(VoskBatchRecognizer *recognizer, int nlsml)
-{
+void vosk_batch_recognizer_set_nlsml(VoskBatchRecognizer *recognizer,
+                                     int nlsml) {
 #if HAVE_CUDA
-    ((BatchRecognizer *)recognizer)->SetNLSML((bool)nlsml);
+  ((BatchRecognizer *)recognizer)->SetNLSML((bool)nlsml);
 #endif
 }
 
-void vosk_batch_recognizer_finish_stream(VoskBatchRecognizer *recognizer)
-{
+void vosk_batch_recognizer_finish_stream(VoskBatchRecognizer *recognizer) {
 #if HAVE_CUDA
-    ((BatchRecognizer *)recognizer)->FinishStream();
+  ((BatchRecognizer *)recognizer)->FinishStream();
 #endif
 }
 
-const char *vosk_batch_recognizer_front_result(VoskBatchRecognizer *recognizer)
-{
+const char *
+vosk_batch_recognizer_front_result(VoskBatchRecognizer *recognizer) {
 #if HAVE_CUDA
-    return ((BatchRecognizer *)recognizer)->FrontResult();
+  return ((BatchRecognizer *)recognizer)->FrontResult();
 #else
-    return NULL;
+  return NULL;
 #endif
 }
 
-void vosk_batch_recognizer_pop(VoskBatchRecognizer *recognizer)
-{
+void vosk_batch_recognizer_pop(VoskBatchRecognizer *recognizer) {
 #if HAVE_CUDA
-    ((BatchRecognizer *)recognizer)->Pop();
+  ((BatchRecognizer *)recognizer)->Pop();
 #endif
 }
 
-
-int vosk_batch_recognizer_get_pending_chunks(VoskBatchRecognizer *recognizer)
-{
+int vosk_batch_recognizer_get_pending_chunks(VoskBatchRecognizer *recognizer) {
 #if HAVE_CUDA
-    return ((BatchRecognizer *)recognizer)->GetNumPendingChunks();
+  return ((BatchRecognizer *)recognizer)->GetNumPendingChunks();
 #else
-    return 0;
+  return 0;
 #endif
 }
