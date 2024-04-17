128a7043c081a7a4cf9bcd48f76b5c194a48837c
Binozo <entwickler@binozoworks.de>
2024-04-17 23:47:45 +0200

    updated example

--- go/example/test_simple.go
+++ go/example/test_simple.go
@@ -16,10 +16,7 @@ func main() {
 	flag.StringVar(&filename, "f", "", "file to transcribe")
 	flag.Parse()
 
-	model, err := vosk.NewModel("model")
-	if err != nil {
-		log.Fatal(err)
-	}
+	model := vosk.NewModel("model")
 
 	// we can check if word is in the vocabulary
 	// fmt.Println(model.FindWord("air"))
