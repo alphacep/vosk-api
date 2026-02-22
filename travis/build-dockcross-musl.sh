3c66cf717532588abebfa43a82dd723c59fdd553
uvlad7 <uvlad7@gmail.com>
2026-02-22 16:04:12 +0300

    Vibecode cross-compilation

--- travis/build-dockcross-musl.sh
+++ travis/build-dockcross-musl.sh
@@ -4,4 +4,4 @@ set -e
 set -x
 
 docker build --build-arg="DOCKCROSS_IMAGE=dockcross/linux-armv7l-musl" --build-arg="OPENBLAS_ARGS=TARGET=ARMV7" --file Dockerfile.dockcross-musl --tag alphacep/kaldi-dockcross-armv7-musl:latest .
-docker run --rm -v /home/shmyrev/travis/vosk-api/:/io alphacep/kaldi-dockcross-armv7-musl /io/travis/build-wheels-dockcross.sh
+docker run --rm -v `realpath ..`:/io alphacep/kaldi-dockcross-armv7-musl /io/travis/build-wheels-dockcross.sh
