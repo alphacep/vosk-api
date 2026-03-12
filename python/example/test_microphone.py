871a629c2185d1b9a69c2526967bed03aa3db5e1
Oleg Stepanov <oleg.stepanov@gmail.com>
2026-03-12 16:21:00 +0300

    More options in test_microphone.py

--- python/example/test_microphone.py
+++ python/example/test_microphone.py
@@ -5,13 +5,22 @@
 # For more help run: `python test_microphone.py -h`
 
 import argparse
+from dataclasses import dataclass
+import json
 import queue
 import sys
 import sounddevice as sd
+from typing import Any
 
 from vosk import Model, KaldiRecognizer
 
 q = queue.Queue()
+
+@dataclass(kw_only=True)
+class Result:
+    text: str
+    is_partial: bool
+    raw: Any
 
 def int_or_str(text):
     """Helper function for argument parsing."""
@@ -48,6 +57,12 @@ parser.add_argument(
     "-r", "--samplerate", type=int, help="sampling rate")
 parser.add_argument(
     "-m", "--model", type=str, help="language model; e.g. en-us, fr, nl; default is en-us")
+parser.add_argument(
+    "-se", "--skip-empty", action="store_true", help="Skip empty partial results.")
+parser.add_argument(
+    "--nlsml", action="store_true", help="Generate output in NLSML (Natural Language Semantics Markup Language). Disables pretty print.")
+parser.add_argument(
+    "-pp", "--prettyprint", action="store_true", help="Enable pretty (less verbose) output")
 args = parser.parse_args(remaining)
 
 try:
@@ -66,6 +81,8 @@ try:
     else:
         dump_fn = None
 
+    pretty_print = args.prettyprint
+
     with sd.RawInputStream(samplerate=args.samplerate, blocksize = 8000, device=args.device,
             dtype="int16", channels=1, callback=callback):
         print("#" * 80)
@@ -73,14 +90,49 @@ try:
         print("#" * 80)
 
         rec = KaldiRecognizer(model, args.samplerate)
+        if args.nlsml:
+            rec.SetMaxAlternatives(10)
+            rec.SetNLSML(True)
+            pretty_print = False
+
         while True:
             data = q.get()
             if rec.AcceptWaveform(data):
-                print(rec.Result())
+                raw_result = rec.Result()
+                try:
+                    result_json = json.loads(raw_result)
+                except:
+                    result_json = None
+
+                result = Result(
+                    is_partial=False,
+                    raw=raw_result,
+                    text=result_json["text"] if result_json else "", 
+                )
             else:
-                print(rec.PartialResult())
+                raw_result = rec.PartialResult()
+                result_json = json.loads(raw_result)                
+                result = Result(
+                    is_partial=True,
+                    raw=raw_result,
+                    text=result_json["partial"], 
+                )
+
             if dump_fn is not None:
                 dump_fn.write(data)
+
+            if args.skip_empty and result.is_partial and not result.text:
+                continue
+
+            if pretty_print:
+                if result.is_partial and not result.text:
+                    print(".", end="", flush=True)
+                else:
+                    print("> " if result.is_partial else "# ", end="")
+                    print(result.text)
+            else:
+                print(result.raw)
+            
 
 except KeyboardInterrupt:
     print("\nDone")
